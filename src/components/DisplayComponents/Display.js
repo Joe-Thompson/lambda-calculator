844bc33b7ee297e8cd7f75fbaaf8ecbeb4af9308
Joe Thompson <joe@pop-os.localdomain>
2019-08-26 18:16:29 -0400

    basic styling

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+// const [num, updateNum] = useState(numbers);
+
+const Display = (props) => {
+  return <div className="display">{props.number}</div>;
 };
+
+export default Display;
